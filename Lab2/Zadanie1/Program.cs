581824be4959c4b64c898d487be9574f4b2aab0e
eseppenen <elik_7708@mail.ru>
2017-10-18 20:30:27 +0600

    лаб2 задание1

---
+++ Lab2/Zadanie1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String str;
+            str = Console.ReadLine();
+            double X;
+            X = double.Parse(str);
+            double result;
+            result = X + 7;
+            Console.WriteLine("{0:F4}", result);
+
+
+
+
+        }
+    }
+}
